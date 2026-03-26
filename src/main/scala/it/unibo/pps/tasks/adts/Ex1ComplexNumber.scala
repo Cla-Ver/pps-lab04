b6f43f5262b8546f6a2ecb671edf237c66c02b8b
Claudio Verazza - claudio.verazza@studio.unibo.it <claudio.verazza@studio.unibo.it>
2026-03-26 10:26:45 +0100

    Complex works

--- src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/it/unibo/pps/tasks/adts/Ex1ComplexNumber.scala
@@ -20,11 +20,20 @@ object Ex1ComplexNumbers:
   object BasicComplexADT extends ComplexADT:
 
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    case class complexNumber(real: Double, imaginary: Double)
+
+    type Complex = complexNumber
+    def complex(re: Double, im: Double): Complex = complexNumber(re, im)
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex.real
+      def im(): Double = complex.imaginary
+      def sum(other: Complex): Complex = complexNumber(complex.real + other.real, complex.imaginary + other.imaginary)
+      def subtract(other: Complex): Complex = complexNumber(complex.real - other.real, complex.imaginary - other.imaginary)
+      def asString(): String = (complex.real, complex.imaginary) match
+        case (r, i) if r != 0 && i > 0 => r.toString() + " + " + math.abs(i) + "i"
+        case (r, i) if r != 0 && i < 0 => r.toString() + " - " + math.abs(i) + "i"
+        case (r, i) if r == 0 && i != 0 => i.toString() + "i"
+        case _ => complex.real.toString()
+/*case (r, i) if n > 0 => complex.real.toString() + " + " + math.abs(n) + "i"
+case n if n < 0 => complex.real.toString() + " - " + math.abs(n) + "i"
+case _ => complex.real.toString()*/
